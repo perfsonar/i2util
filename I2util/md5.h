86ee71f6eb18fb9225255b0b83b1e16fc9eed0e3
aaron <aaron@af504c07-53ac-4b46-a7f1-a0422916b9a2>
2012-08-24 18:25:45 +0000

    Update the I2util md5 implementation to use one that doesn't have a bizarre license
    git-svn-id: http://anonsvn.internet2.edu/svn/I2util/trunk@215 af504c07-53ac-4b46-a7f1-a0422916b9a2

--- I2util/md5.h
+++ I2util/md5.h
@@ -1,51 +1,45 @@
-/* MD5.H - header file for MD5C.C
- * $FreeBSD: src/sys/sys/md5.h,v 1.13 1999/12/29 04:24:44 peter Exp $
+/*
+ * This is an OpenSSL-compatible implementation of the RSA Data Security, Inc.
+ * MD5 Message-Digest Algorithm (RFC 1321).
+ *
+ * Homepage:
+ * http://openwall.info/wiki/people/solar/software/public-domain-source-code/md5
+ *
+ * Author:
+ * Alexander Peslyak, better known as Solar Designer <solar at openwall.com>
+ *
+ * This software was written by Alexander Peslyak in 2001.  No copyright is
+ * claimed, and the software is hereby placed in the public domain.
+ * In case this attempt to disclaim copyright and place the software in the
+ * public domain is deemed null and void, then the software is
+ * Copyright (c) 2001 Alexander Peslyak and it is hereby released to the
+ * general public under the following terms:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ *
+ * See md5.c for more information.
  */
 
-/* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
-rights reserved.
+#ifdef HAVE_OPENSSL
+#include <openssl/md5.h>
+#elif !defined(_MD5_H)
+#define _MD5_H
 
-License to copy and use this software is granted provided that it
-is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-Algorithm" in all material mentioning or referencing this software
-or this function.
+/* Any 32-bit or wider unsigned integer data type will do */
+typedef unsigned int MD5_u32plus;
 
-License is also granted to make and use derivative works provided
-that such works are identified as "derived from the RSA Data
-Security, Inc. MD5 Message-Digest Algorithm" in all material
-mentioning or referencing the derived work.
-
-RSA Data Security, Inc. makes no representations concerning either
-the merchantability of this software or the suitability of this
-software for any particular purpose. It is provided "as is"
-without express or implied warranty of any kind.
-
-These notices must be retained in any copies of any part of this
-documentation and/or software.
- */
-
-#ifndef _I2_md5_h_
-#define _I2_md5_h_
-#include <I2util/util.h>
-
-/* MD5 context. */
-typedef struct I2MD5Context {
-  uint32_t state[4];	/* state (ABCD) */
-  uint32_t count[2];	/* number of bits, modulo 2^64 (lsb first) */
-  unsigned char buffer[64];	/* input buffer */
+typedef struct {
+	MD5_u32plus lo, hi;
+	MD5_u32plus a, b, c, d;
+	unsigned char buffer[64];
+	MD5_u32plus block[16];
 } I2MD5_CTX;
 
+extern void I2MD5Init(I2MD5_CTX *ctx);
+extern void I2MD5Update(I2MD5_CTX *ctx, void *data, unsigned long size);
+extern void I2MD5Final(unsigned char *result, I2MD5_CTX *ctx);
 
-BEGIN_C_DECLS
-
-void   I2MD5Init (I2MD5_CTX *);
-void   I2MD5Update (I2MD5_CTX *, const unsigned char *, unsigned int);
-void   I2MD5Pad (I2MD5_CTX *);
-void   I2MD5Final (unsigned char [16], I2MD5_CTX *);
-char * I2MD5End(I2MD5_CTX *, char *);
-char * I2MD5File(const char *, char *);
-char * I2MD5Data(const unsigned char *, unsigned int, char *);
-
-END_C_DECLS
-
-#endif /* _I2_md5_h_ */
+#endif
