c2f410a80ca3251401811d670d9a3e95d9d3ef40
aaron <aaron@af504c07-53ac-4b46-a7f1-a0422916b9a2>
2012-10-16 19:57:01 +0000

    Add license stuff to the various .c files
    git-svn-id: http://anonsvn.internet2.edu/svn/I2util/trunk@216 af504c07-53ac-4b46-a7f1-a0422916b9a2

--- I2util/readpassphrase.h
+++ I2util/readpassphrase.h
@@ -18,6 +18,21 @@
  *
  *	Description:	
  *		pulled-in from freebsd.
+ *		The Internet2 copyright only applies to changes made
+ *		to this code. The original copyright follows.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 /*
  * Copyright (c) 2000 Todd C. Miller <Todd.Miller@courtesan.com>
--- I2util/sha1.c
+++ I2util/sha1.c
@@ -23,6 +23,22 @@
  *    Public domain version of SHA-1 from 'entropy'. Modifying
  *    it as needed... (Will change to I2 symbols - and update for C99
  *    compliance over time.)
+ *
+ *		The Internet2 copyright only applies to changes made
+ *		to this code. The original copyright follows.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 /*
  * SHA-1 in C
